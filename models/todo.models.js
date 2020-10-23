cd95b775b07d9ff932455b370ac04c6b3eaf47ca
Avet Brutyan <awet.brutjan@gmail.com>
2020-10-23 04:03:04 +0400

    Timestamps in DB

--- models/todo.models.js
+++ models/todo.models.js
@@ -5,8 +5,9 @@ const TodoSchema = new mongoose.Schema(
     text: {
       type: String,
       required: true,
-    }
-  }
+    },
+  },
+  { timestamps: true }
 );
 
 const Todo = mongoose.model("Todo", TodoSchema);
